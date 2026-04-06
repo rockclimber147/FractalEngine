acefd5b47716fe048baa97a01fffde5d24a559c1
Daylen <daylencsmith@gmail.com>
2026-04-06 13:38:38 -0700

    add selector for sliders

--- include/App.hpp
+++ include/App.hpp
@@ -4,10 +4,19 @@
 #include "AmountSliderComponent.hpp"
 
 class App {
+public:
+    enum class ColorChannel {
+            Red,
+            Green,
+            Blue
+        };
+
 private:
     AmountSliderComponent m_red;
     AmountSliderComponent m_green;
     AmountSliderComponent m_blue;
+
+    ColorChannel m_activeChannel = ColorChannel::Red;
 
 public:
     // Constructor with Initializer List
--- src/App.cpp
+++ src/App.cpp
@@ -6,10 +6,27 @@
 void App::Run() {
     // Control Window
     ImGui::Begin("Control Panel");
+
+    ImGui::Text("Select Active Slider:");
+    if (ImGui::RadioButton("Red", (int*)&m_activeChannel, (int)ColorChannel::Red)) {} 
+    ImGui::SameLine();
+    if (ImGui::RadioButton("Green", (int*)&m_activeChannel, (int)ColorChannel::Green)) {} 
+    ImGui::SameLine();
+    if (ImGui::RadioButton("Blue", (int*)&m_activeChannel, (int)ColorChannel::Blue)) {}
+
+    ImGui::Separator();
     
-    m_red.DrawControlPanel();
-    m_green.DrawControlPanel();
-    m_blue.DrawControlPanel();
+    switch (m_activeChannel) {
+        case ColorChannel::Red:
+            m_red.DrawControlPanel();
+            break;
+        case ColorChannel::Green:
+            m_green.DrawControlPanel();
+            break;
+        case ColorChannel::Blue:
+            m_blue.DrawControlPanel();
+            break;
+    }
     
     // Capture the current state of our components
     ImVec4 mixColor = ImVec4(
