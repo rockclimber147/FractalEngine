18c13c8f089a5b876e4c3b721f1f048fe8ec2291
Daylen <daylencsmith@gmail.com>
2026-04-10 14:14:39 -0700

    add sliders to tweak barnsley fern

--- include/fractals/CPUBarnsleyFern.hpp
+++ include/fractals/CPUBarnsleyFern.hpp
@@ -1,6 +1,15 @@
 #include "fractals/core/CPUFractalComponent.hpp"
 
 class CPUBarnsleyFern : public CPUFractalComponent {
+protected:
+    float m_mainScale = 0.85f;
+    float m_mainCurvature = 0.04f;
+    float m_mainHeight = 1.6f;
+
+    float m_sideScale = 0.23f;
+    float m_sideRotation = 0.24f;
+    
+    int m_iterationCount = 2000000;
 
 public:
     CPUBarnsleyFern(std::string name);
--- src/fractals/CPUBarnsleyFern.cpp
+++ src/fractals/CPUBarnsleyFern.cpp
@@ -10,39 +10,50 @@ void CPUBarnsleyFern::GenerateFractalData() {
     double x = 0, y = 0;
     double ppu = GetPixelsPerUnit();
     
-    // Run the "Chaos Game" for a few million iterations
-    for (int i = 0; i < 2000000; i++) {
+    for (int i = 0; i < m_iterationCount; i++) {
         double r = (double)rand() / RAND_MAX;
         double nextX, nextY;
 
-        // Barnsley's 4 magic transformations
-        if (r < 0.01) {
+        if (r < 0.01) { // Stem
             nextX = 0;
             nextY = 0.16 * y;
-        } else if (r < 0.86) {
-            nextX = 0.85 * x + 0.04 * y;
-            nextY = -0.04 * x + 0.85 * y + 1.6;
-        } else if (r < 0.93) {
+        } else if (r < 0.86) { // Main Body
+            nextX = m_mainScale * x + m_mainCurvature * y;
+            nextY = -m_mainCurvature * x + m_mainScale * y + m_mainHeight;
+        } else if (r < 0.93) { // Left Leaflet
             nextX = 0.2 * x - 0.26 * y;
             nextY = 0.23 * x + 0.22 * y + 1.6;
-        } else {
+        } else { // Right Leaflet
             nextX = -0.15 * x + 0.28 * y;
             nextY = 0.26 * x + 0.24 * y + 0.44;
         }
+        
         x = nextX; y = nextY;
 
-        // Convert world coordinates (x, y) to pixel coordinates (px, py)
         int px = (int)((x - m_offsetX) * ppu + m_width / 2);
         int py = (m_height - 1) - (int)((y - m_offsetY) * ppu + m_height / 2);
 
-        // If the point is on screen, "paint" it green
         if (px >= 0 && px < m_width && py >= 0 && py < m_height) {
             int idx = (py * m_width + px) * 3;
-            m_pixelBuffer[idx + 1] = 200; // Green channel
+            m_pixelBuffer[idx + 1] = 200; 
         }
     }
 }
 
 void CPUBarnsleyFern::DrawControlPanel() {
+    bool changed = false;
+    
+    ImGui::Text("Main Stem (T2)");
+    if (ImGui::SliderFloat("Scale", &m_mainScale, 0.5f, 0.95f)) changed = true;
+    if (ImGui::SliderFloat("Curvature", &m_mainCurvature, -0.2f, 0.2f)) changed = true;
+    if (ImGui::SliderFloat("Internode", &m_mainHeight, 0.5f, 3.0f)) changed = true;
 
+    ImGui::Separator();
+    ImGui::Text("Leaflet Growth (T3/T4)");
+    if (ImGui::SliderFloat("Side Scale", &m_sideScale, 0.1f, 0.5f)) changed = true;
+    
+    ImGui::Separator();
+    if (ImGui::SliderInt("Points", &m_iterationCount, 100000, 5000000)) changed = true;
+
+    if (changed) UpdateTexture();
 }
